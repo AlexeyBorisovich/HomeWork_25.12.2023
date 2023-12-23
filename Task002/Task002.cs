b7ee100508bef112268f5a2b224cdd5999d1390a
AlexeyBorisovich <a.senior@internet.ru>
2023-12-24 01:53:12 +0600

    Решение задания №2

---
+++ Task002/Task002.cs
@@ -0,0 +1,37 @@
+﻿
+
+// Задача 2: 
+// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string originalString = "Hello WoRlD";
+        string lowercaseString = ConvertToUpperToLower(originalString);
+
+        Console.WriteLine("Исходная строка: " + originalString);
+        Console.WriteLine("Строка с заменой на строчные буквы: " + lowercaseString);
+    }
+
+    static string ConvertToUpperToLower(string input)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            if (char.IsUpper(charArray[i]))
+            {
+                charArray[i] = char.ToLower(charArray[i]);
+            }
+        }
+
+        return new string(charArray);
+    }
+}
